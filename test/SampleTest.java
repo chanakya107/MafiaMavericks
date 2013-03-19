f757182596fdb6cbd4402777601fb21cab0111dc
chanakya107 <chanakg@thoughtworks.com>
2013-03-19 17:46:58 +0530

    Added test folder as it ws deleted by mistake

---
+++ test/SampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+        @Test
+        public void passingTest(){
+
+            Assert.assertTrue("Initial Passing test",true);
+        }
+}
