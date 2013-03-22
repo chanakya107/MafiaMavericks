51f7cc034d29f4bfd1abd0c09d6ec2158c7c4f6c
chanakya107 <chanakg@thoughtworks.com>
2013-03-22 13:24:58 +0530

    Added sample test to test dir as empty dir cannot be pushed and thus Ci-server fails| chanakya\chethan

---
+++ test/SampleTest.java
@@ -0,0 +1,10 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void samplePassingTest(){
+
+        Assert.assertTrue("Initial passing test", true);
+    }
+}
