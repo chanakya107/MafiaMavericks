1e3134f0cc4c0da96f58aefc1d6e2ef7b64fa7a2
Chethan A <chethana@thoughtworks.com>
2013-03-29 14:28:14 +0530

    wrote tests for arrival of ServerDisconnectedMessage, RoleAssignedMessage as Mafia and villager | chethan

--- test/controllers/client/JoinGameControllerTest.java
+++ test/controllers/client/JoinGameControllerTest.java
@@ -2,14 +2,15 @@ package controllers.client;
 
 import channels.SocketChannel;
 import controllers.Workflow;
+import controllers.server.Role;
 import messages.PlayersUpdateMessage;
+import messages.RoleAssignedMessage;
+import messages.ServerDisconnectedMessage;
 import org.junit.Before;
 import org.junit.Test;
 import view.client.JoinGameView;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class JoinGameControllerTest {
     private Workflow workflow;
@@ -63,12 +64,36 @@ public class JoinGameControllerTest {
     }
 
     @Test
-    public void on_new_PlayersUpdatedMessage_arrived_will_display_the_players_updated(){
+    public void on_new_PlayersUpdateMessage_arrived_the_players_will_be_displayed(){
         PlayersUpdateMessage playersUpdateMessage = mock(PlayersUpdateMessage.class);
         String[] players = {"player"};
         when(playersUpdateMessage.getPlayersConnected()).thenReturn(players);
         controller.onNewMessageArrived(channel,playersUpdateMessage);
         verify(view).displayPlayers(players);
     }
+    @Test
+    public void on_new_ServerDisconnectedMessage_arrived_serverDisconnected_will_displayed_and_channel_will_be_stopped(){
+        ServerDisconnectedMessage serverDisconnectedMessage = mock(ServerDisconnectedMessage.class);
+        controller.onNewMessageArrived(channel,serverDisconnectedMessage);
+        verify(view).serverDisconnected("localhost");
+        verify(channel).stop();
+    }
+
+    @Test
+    public void when_a_new_RoleAssignedMessage_is_arrived_with_role_as_mafia_then_mafia_screen_is_displayed(){
+        RoleAssignedMessage roleAssignedMessage = mock(RoleAssignedMessage.class);
+        when(roleAssignedMessage.getRole()).thenReturn(Role.Mafia);
+        controller.onNewMessageArrived(channel, roleAssignedMessage);
+        verify(view).goToMafiaScreen();
+    }
+
+    @Test
+    public void when_a_new_RoleAssignedMessage_is_arrived_with_role_as_villager_then_villager_screen_is_displayed(){
+        RoleAssignedMessage roleAssignedMessage = mock(RoleAssignedMessage.class);
+        when(roleAssignedMessage.getRole()).thenReturn(Role.Villager);
+        controller.onNewMessageArrived(channel, roleAssignedMessage);
+        verify(view).goToVillagerScreen();
+    }
+
 
 }
