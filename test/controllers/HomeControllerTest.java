d49bd8325575e9a039afb4c8c7061e4d13f8abc9
Chethan A <chethana@thoughtworks.com>
2013-04-08 19:58:15 +0530

    added test to homeController for displaying the server disconnect in the homescreen | chethan | 1

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,24 +1,39 @@
 package controllers;
 
+import org.junit.Before;
 import org.junit.Test;
+import view.HomeView;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class HomeControllerTest {
+    Workflow workflow;
+        HomeController controller;
+    @Before
+    public void setup(){
+        workflow = mock(Workflow.class);
+        controller = new HomeController(workflow);
+    }
+
     @Test
     public void startServer_should_display_startServerScreen() {
-        Workflow workflow = mock(Workflow.class);
-        HomeController controller = new HomeController(workflow);
         controller.startServer();
         verify(workflow).startServer();
     }
 
     @Test
     public void getPlayerDetails_should_display_startServerScreen() {
-        Workflow workflow = mock(Workflow.class);
-        HomeController controller = new HomeController(workflow);
         controller.getPlayerDetails();
         verify(workflow).getGameDetails();
     }
+
+    @Test
+    public void when_server_disconnected_connection_to_server_lost_should_be_displayed(){
+        HomeView view = mock(HomeView.class);
+        controller.bind(view);
+        controller.serverDisconnected("player");
+        verify(view).serverDisconnected("player");
+    }
+
 }
