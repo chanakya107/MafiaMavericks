d001a209e56c9efd946e772d9129738bde15d7d2
CiHostMafia <cihost.mafia@gmail.com>
2013-04-09 09:03:37 +0530

    added gotoHome button to gameOver screen|deepthi-raghavendra

--- src/Screens/GameOverScreen.java
+++ src/Screens/GameOverScreen.java
@@ -7,16 +7,31 @@ import view.GameOverView;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameOverScreen implements GameOverView {
     private final MainFrame mainFrame;
     private final GameOverController controller;
     private final JPanel panel;
 
-    public GameOverScreen(MainFrame mainFrame, GameOverController controller) {
+    public GameOverScreen(MainFrame mainFrame, final GameOverController controller) {
         this.mainFrame = mainFrame;
         this.controller = controller;
         panel = mainFrame.createPanel("findwallpaper.info.jpg");
+        JButton goToHome = new JButton("Go To Home");
+        panel.add(goToHome);
+        goToHome.setFont(new Font("Comic Sans Ms", Font.PLAIN, 25));
+        goToHome.setBackground(Color.BLACK);
+        goToHome.setForeground(Color.white);
+        goToHome.setBounds(1000, 700, 180, 40);
+
+        goToHome.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.goToHome();
+            }
+        });
     }
 
     @Override
--- src/controllers/GameOverController.java
+++ src/controllers/GameOverController.java
@@ -21,4 +21,8 @@ public class GameOverController {
     public void start() {
         view.displayWinner(winner);
     }
+
+    public void goToHome() {
+        workflow.goToHome();
+    }
 }
